4c817915c0f1320498bf3a9ed5da07f010e90556
jackturner <jack@jackturner.design>
2019-04-11 13:43:35 -0400

    Favicon stuff. Close #6

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,10 +35,11 @@ module.exports = {
     ],
 	},
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik:400,500' }]
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik:400,500' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico', color: '#33272C' }]
   ]
 }
-
-
-
-
